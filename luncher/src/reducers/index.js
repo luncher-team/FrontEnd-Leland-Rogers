f6c30866d6f6fa6a918562857edb5712bcd83e76
ltrii <lelandtr@gmail.com>
2019-02-04 09:13:22 -0800

    branch init

---
+++ luncher/src/reducers/index.js
@@ -0,0 +1,11 @@
+
+
+const initialState = {
+    schoolFetch: false,
+    schoolsFetched: false,
+    schoolsFetchFail: false,
+    donationFetch: false,
+    addDonation: false,
+    donationAdded: false,
+    error: null
+}
